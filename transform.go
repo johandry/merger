4039c811234ecadfc0d3e526530fb1b69c411d81
Johandry Amador <johandry@gmail.com>
2019-07-21 21:11:01 -0700

    Refactoring & Adding `TransformToMap` function

---
+++ transform.go
@@ -0,0 +1,226 @@
+package merger
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+const defaultTagName = "json"
+
+// TransformMap transform a map of string values to interface{} values
+func TransformMap(srcMap map[string]string) map[string]interface{} {
+	m := make(map[string]interface{}, 0)
+	for k, v := range srcMap {
+		var i interface{}
+		switch {
+		case isSlice(v):
+			i = transformToSlice(v)
+		case isJSONStruct(v):
+			i = transformJSONToStruct(v)
+		default:
+			i = v
+		}
+
+		if isStructField(k) {
+			m = transformToStructField(m, k, i)
+		} else {
+			if m[k] != nil && isMap(i) {
+				m[k] = mergeTwoMaps(m[k].(map[string]interface{}), i.(map[string]interface{}), false)
+			} else {
+				m[k] = i
+			}
+		}
+	}
+
+	return m
+}
+
+func isSlice(v string) bool {
+	return (strings.Contains(v, ",") || (strings.HasPrefix(v, "[") && strings.HasSuffix(v, "]"))) && !isJSONStruct(v)
+}
+func transformToSlice(v string) []string {
+	v = strings.Trim(v, "[ ]")
+	strs := strings.Split(v, ",")
+	values := make([]string, 0)
+	for _, str := range strs {
+		str = strings.Trim(str, " ")
+		str = strings.Trim(str, "'")
+		values = append(values, str)
+	}
+	return values
+}
+
+func isStructField(k string) bool {
+	return strings.Contains(k, FieldSeparator)
+}
+func transformToStructField(m map[string]interface{}, k string, v interface{}) map[string]interface{} {
+	if isStructField(k) {
+		keys := strings.Split(k, FieldSeparator)
+		k0 := keys[0]
+		r := strings.Join(keys[1:], FieldSeparator)
+
+		if _, ok := m[k0]; !ok {
+			m[k0] = make(map[string]interface{}, 0)
+		}
+
+		m[k0] = transformToStructField(m[k0].(map[string]interface{}), r, v)
+		return m
+	}
+
+	if _, ok := m[k]; !ok {
+		m[k] = v
+		return m
+	}
+
+	if m[k] != nil && isMap(v) {
+		m[k] = mergeTwoMaps(m[k].(map[string]interface{}), v.(map[string]interface{}), false)
+		return m
+	}
+
+	m[k] = v
+	return m
+}
+
+func isJSONStruct(v string) bool {
+	return strings.HasPrefix(v, "{") && strings.HasSuffix(v, "}")
+}
+func transformJSONToStruct(v string) map[string]interface{} {
+	var m map[string]interface{}
+	// Ignore error, if it's not a valid JSON return an empty map
+	json.Unmarshal([]byte(v), &m)
+	return m
+}
+
+func isMap(v interface{}) bool {
+	t := reflect.TypeOf(v).String()
+	return t == "map[string]interface {}" || t == "map[string]string"
+}
+
+func mergeTwoMaps(dstMap, srcMap map[string]interface{}, overwrite bool) map[string]interface{} {
+	for k := range srcMap {
+		// dstMap[k] doesn't have a value
+		if _, ok := dstMap[k]; !ok {
+			dstMap[k] = srcMap[k]
+			continue
+		}
+		// dstMap[k] and srcMap[k] are both maps, merge them
+		if isMap(dstMap[k]) && isMap(srcMap[k]) {
+			dstMap[k] = mergeTwoMaps(dstMap[k].(map[string]interface{}), srcMap[k].(map[string]interface{}), overwrite)
+			continue
+		}
+		// dstMap[k] or srcMap[k] is a maps and the other is not,
+		// or both are not maps, force the assigment if overwrite
+		if overwrite {
+			dstMap[k] = srcMap[k]
+		}
+	}
+
+	return dstMap
+}
+
+// TransformToMap returns the given interface (has to be a struct) as a set of
+// variables + values map. Useful to get the environment variables or parameters
+// of a given struct before merge it with other struct
+func TransformToMap(v interface{}, tagNames ...string) (map[string]string, error) {
+	m := map[string]string{}
+
+	if v == nil {
+		return m, nil
+	}
+
+	ptrRef := reflect.ValueOf(v)
+	if ptrRef.Kind() != reflect.Ptr {
+		return m, fmt.Errorf("invalid value, it's not a struct pointer, it's a %s. %v", ptrRef.Kind().String(), ptrRef)
+	}
+	ref := ptrRef.Elem()
+	if ref.Kind() != reflect.Struct {
+		return m, fmt.Errorf("invalid value, it's not a struct, it's a %s. %v", ref.Kind().String(), ref)
+	}
+
+	if len(tagNames) == 0 {
+		tagNames = []string{defaultTagName}
+	}
+
+	return parseStruct("", ref, m, tagNames), nil
+}
+
+func parseStruct(parent string, val reflect.Value, m map[string]string, tagNames []string) map[string]string {
+	valType := val.Type()
+	for i := 0; i < valType.NumField(); i++ {
+		refTypeField := valType.Field(i)
+		name, ignore := getName(refTypeField, tagNames)
+		if ignore {
+			continue
+		}
+
+		if len(parent) != 0 {
+			name = parent + FieldSeparator + name
+		}
+
+		// Because all the names are lower case. Case does not matter
+		name = strings.ToLower(name)
+		valField := val.Field(i)
+		m = appendTo(name, m, valField, tagNames)
+	}
+
+	return m
+}
+
+func getName(field reflect.StructField, tagNames []string) (name string, ignore bool) {
+	// Try the given keys, in order of importance ...
+	for _, tagName := range tagNames {
+		var ok bool
+		if name, ok = field.Tag.Lookup(tagName); ok && len(name) != 0 && name != "-" {
+			// if this tag is not found or contain an empty or `-` value, try the next tag
+			// otherwise, end the loop bc the name have been found
+			return name, false
+		}
+		if name == "-" {
+			// If the tag name is `-` it will be ignored
+			return "", true
+		}
+	}
+
+	// If any tag is found, or they are empty and are not `-`, return the field name
+	return field.Name, false
+}
+
+func appendTo(name string, m map[string]string, v reflect.Value, tagNames []string) map[string]string {
+	val := reflect.ValueOf(v.Interface())
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	switch val.Kind() {
+	case reflect.Struct:
+		m = parseStruct(name, val, m, tagNames)
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			keyStr := fmt.Sprintf("%v", key.Interface())
+			keyStr = strings.Replace(keyStr, " ", "_", -1)
+			n := name + FieldSeparator + keyStr
+			m = appendTo(n, m, val.MapIndex(key), tagNames)
+		}
+	case reflect.Slice, reflect.Array:
+		switch val.Type().Elem().Kind() {
+		case reflect.Struct, reflect.Ptr, reflect.Map, reflect.Slice, reflect.Array:
+			// TODO: At this time only a slice/array of simple type are possible to map
+			// return m, fmt.Errorf("cannot map a slice/array of struct, map or slice/array")
+		default:
+			list := "["
+			for i := 0; i < val.Len()-1; i++ {
+				list = list + fmt.Sprintf("%v", val.Index(i).Interface()) + ", "
+			}
+			i := val.Len() - 1
+			list = list + fmt.Sprintf("%v]", val.Index(i).Interface())
+			m[name] = list
+		}
+	default:
+		value := v.Interface()
+		m[name] = fmt.Sprintf("%v", value)
+	}
+
+	return m
+}
---
+++ transform_test.go
@@ -0,0 +1,310 @@
+package merger_test
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/johandry/merger"
+)
+
+func TestTransformMap(t *testing.T) {
+	type args struct {
+		srcMap map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]interface{}
+	}{
+		{name: "Simple",
+			args: args{srcMap: map[string]string{"F1": "20", "F2": "Text", "F3": "True"}},
+			want: map[string]interface{}{"F1": "20", "F2": "Text", "F3": "True"},
+		},
+		{name: "Slices",
+			args: args{srcMap: map[string]string{
+				"OneItem":  "[Item]",
+				"Books":    "  B1,B2, B3",
+				"Articles": "[A1, A2, A3]  ",
+				"Items":    "  [  I1  , I2,  'Item number #3'   ,   I4  ]  ",
+			}},
+			want: map[string]interface{}{
+				"OneItem":  []string{"Item"},
+				"Books":    []string{"B1", "B2", "B3"},
+				"Articles": []string{"A1", "A2", "A3"},
+				"Items":    []string{"I1", "I2", "Item number #3", "I4"},
+			},
+		},
+		{name: "Structs",
+			args: args{srcMap: map[string]string{
+				"Address__City":            "New York",
+				"Address__Country":         "US",
+				"Parents__Address__Zip":    "32123",
+				"Parents__Address__Planet": "Earth",
+			}},
+			want: map[string]interface{}{
+				"Address": map[string]interface{}{
+					"City":    "New York",
+					"Country": "US",
+				},
+				"Parents": map[string]interface{}{
+					"Address": map[string]interface{}{
+						"Zip":    "32123",
+						"Planet": "Earth",
+					},
+				},
+			},
+		},
+		{name: "Mixed",
+			args: args{srcMap: map[string]string{"IP": "192.168.1.0", "DNS__Servers": "[192.168.0.1, 192.168.0.2, 192.168.0.3]"}},
+			want: map[string]interface{}{
+				"IP": "192.168.1.0",
+				"DNS": map[string]interface{}{
+					"Servers": []string{"192.168.0.1", "192.168.0.2", "192.168.0.3"},
+				},
+			},
+		},
+		{name: "Structs-JSON",
+			args: args{srcMap: map[string]string{
+				"Address": `{"city": "New York", "country": "US"}`,
+				"Parents": `{"address": {"zip": "32123", "planet": "Earth"}}`,
+			}},
+			want: map[string]interface{}{
+				"Address": map[string]interface{}{
+					"city":    "New York",
+					"country": "US",
+				},
+				"Parents": map[string]interface{}{
+					"address": map[string]interface{}{
+						"zip":    "32123",
+						"planet": "Earth",
+					},
+				},
+			},
+		},
+		{name: "Mixed-JSON",
+			args: args{srcMap: map[string]string{
+				"IP":                    "192.168.1.0",
+				"DNS__Servers":          "[192.168.0.1, 192.168.0.2, 192.168.0.3]",
+				"Parents__Address__Zip": "32123",
+				"Parents":               `{"Address": {"Planet": "Earth"}}`,
+			}},
+			want: map[string]interface{}{
+				"IP": "192.168.1.0",
+				"DNS": map[string]interface{}{
+					"Servers": []string{"192.168.0.1", "192.168.0.2", "192.168.0.3"},
+				},
+				"Parents": map[string]interface{}{
+					"Address": map[string]interface{}{
+						"Zip":    "32123",
+						"Planet": "Earth",
+					},
+				},
+			},
+		},
+	}
+	// assert := assert.New(t)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := merger.TransformMap(tt.args.srcMap); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TransformMap() = %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
+type SimpleWithIgnore struct {
+	F1 int    `json:"age" mapstructure:"-"`
+	F2 string `yaml:"name" mapstructure:"-"`
+}
+
+type Movies struct {
+	DBName string   `json:"movie_database_name"`
+	Movies []*Movie `json:"movies"`
+}
+
+type Movie struct {
+	Title     string                 `json:"title"`
+	Year      int                    `json:"year"`
+	Comment   string                 `json:"-"`
+	Actors    map[string]MoviePerson `json:"actors"`
+	Genres    []string               `json:"genres"`
+	Release   map[string]int         `json:"release_year_per_country"`
+	Directors []MoviePerson          `json:"directors"`
+}
+
+type MoviePerson struct {
+	Name string `json:"full_name"`
+	Age  int    `json:"age"`
+}
+
+var movie01 = Movie{
+	Title:   "Seven Samurai",
+	Year:    1954,
+	Comment: "Good movie, but this comment goes to the void",
+	Actors: map[string]MoviePerson{
+		"Kikuchiyo": MoviePerson{
+			Name: "Toshirô Mifune",
+		},
+		"Kambei Shimada": MoviePerson{
+			Name: "Takashi Shimura",
+		},
+	},
+	Genres: []string{"Adventure", "Drama"},
+	Release: map[string]int{
+		"JP": 1954,
+		"US": 1956,
+	},
+	Directors: []MoviePerson{
+		MoviePerson{Name: "Akira Kurosawa"},
+	},
+}
+var movie01Map = map[string]string{
+	"title":                             "Seven Samurai",
+	"year":                              "1954",
+	"actors__kikuchiyo__full_name":      "Toshirô Mifune",
+	"actors__kikuchiyo__age":            "0",
+	"actors__kambei_shimada__full_name": "Takashi Shimura",
+	"actors__kambei_shimada__age":       "0",
+	"genres":                            "[Adventure, Drama]",
+	"release_year_per_country__JP":      "1954",
+	"release_year_per_country__US":      "1956",
+}
+
+var movie02 = Movie{
+	Title:   "The Godfather",
+	Year:    1972,
+	Comment: "Good movie, but this comment will be ignored",
+	Actors: map[string]MoviePerson{
+		"Don Vito Corleone": MoviePerson{
+			Name: "Marlon Brando",
+		},
+		"Michael Corleone": MoviePerson{
+			Name: "Al Pacino",
+			Age:  time.Now().Year() - 1940,
+		},
+	},
+	Genres: []string{"Crime", "Drama"},
+	Release: map[string]int{
+		"HK": 1973,
+		"US": 1972,
+	},
+	Directors: []MoviePerson{
+		MoviePerson{Name: "Francis Ford Coppola"},
+	},
+}
+var movie02Map = map[string]string{
+	"title":                                "The Godfather",
+	"year":                                 "1972",
+	"actors__don_vito_corleone__full_name": "Marlon Brando",
+	"actors__don_vito_corleone__age":       "0",
+	"actors__michael_corleone__full_name":  "Al Pacino",
+	"actors__michael_corleone__age":        "79",
+	"genres":                               "[Crime, Drama]",
+	"release_year_per_country__HK":         "1973",
+	"release_year_per_country__US":         "1972",
+}
+
+func TestTransformToMap(t *testing.T) {
+	tests := []struct {
+		name    string
+		v       interface{}
+		tags    []string
+		want    map[string]string
+		wantErr bool
+	}{
+		{name: "nil",
+			v:       nil,
+			want:    map[string]string{},
+			wantErr: false,
+		},
+		{name: "no struct (int)",
+			v:       10,
+			want:    map[string]string{},
+			wantErr: true,
+		},
+		{name: "no struct pointer",
+			v:       Simple{1, "foo"},
+			want:    map[string]string{},
+			wantErr: true,
+		},
+		{name: "simple with ignored fields",
+			v: &SimpleWithIgnore{
+				F1: 30,
+				F2: "John",
+			},
+			tags:    []string{"mapstructure", "json", "yaml"},
+			want:    map[string]string{},
+			wantErr: false,
+		},
+		{name: "simple",
+			v: &Simple{
+				F1: 123,
+				F2: "something",
+			},
+			want: map[string]string{
+				"f1": "123",
+				"f2": "something",
+			},
+			wantErr: false,
+		},
+		{name: "complex",
+			v: &Person{
+				Name:    "Pepe",
+				Age:     20,
+				Address: Address{City: "San Diego", Country: "US"},
+				Phones: map[string]Phone{
+					"home":   Phone{Number: "858-123-4567", Available: true},
+					"mobile": Phone{Number: "858-987-6543"},
+				},
+			},
+			want: map[string]string{
+				"name":                      "Pepe",
+				"age":                       "20",
+				"address__city":             "San Diego",
+				"address__country":          "US",
+				"phones__home__number":      "858-123-4567",
+				"phones__home__available":   "true",
+				"phones__mobile__number":    "858-987-6543",
+				"phones__mobile__available": "false",
+			},
+			wantErr: false,
+		},
+		{name: "movies",
+			v: &Movies{
+				DBName: "DBTest",
+				Movies: []*Movie{
+					&movie01,
+					&movie02,
+				},
+			},
+			want: map[string]string{
+				"movie_database_name": "DBTest",
+			},
+			wantErr: false,
+		},
+		{name: "movie01",
+			v:       &movie01,
+			want:    movie01Map,
+			wantErr: false,
+		},
+		{name: "movie02",
+			v:       &movie02,
+			want:    movie02Map,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := merger.TransformToMap(tt.v, tt.tags...)
+			t.Logf("Test %q returns %v", tt.name, got)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("TransformToMap() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TransformToMap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
